1f639003170d2731b9be9f2efd23884de7fe5231
Shivang Kashyap Ankur <91110618+ShivangkashyapAnkur@users.noreply.github.com>
2023-12-13 23:39:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,17 +49,18 @@ app.get("/players/", async (request, response) => {
   );
 });
 app.post("/players/", async (request, response) => {
-    const playersDetails=request.body;
-    const {playerName,jerseyNumber,role}=playersDetails;
-    const addPlayerQuery=`
+  const playersDetails = request.body;
+  const { playerName, jerseyNumber, role } = playersDetails;
+  const addPlayerQuery = `
     INSERT INTO 
     cricket_team (player_name, jersey_number, role)
     VALUES
-    ('${playerName}',${jerseyNumber},'${role}');`;
-    
-    const dbResponse=await db.run(addPlayerQuery);
-    response.send("Player Added to Team");
+    ('${playerName}',
+     ${jerseyNumber},
+      '${role}');`;
 
+  const dbResponse = await db.run(addPlayerQuery);
+  response.send("Player Added to Team");
 });
 
 app.get("/players/:playerId/", async (request, response) => {
@@ -78,17 +79,14 @@ app.get("/players/:playerId/", async (request, response) => {
 app.put("/players/:playerId/", async (request, response) => {
   const { playerId } = request.params;
   const playerDetails = request.body;
-  const {
-   
-    playerName,jerseyNumber,role,
-  } = playerDetails;
+  const { playerName, jerseyNumber, role } = playerDetails;
   const updatePlayerQuery = `
     UPDATE
       cricket_team
     SET
     player_name='${playerName}',
     jersey_number=${jerseyNumber},
-    role='${role}'
+    role='${role}''
      WHERE
       player_id = ${playerId};`;
   await db.run(updatePlayerQuery);
@@ -104,4 +102,5 @@ app.delete("/players/:playerId/", async (request, response) => {
   await db.run(deletePlayerQuery);
   response.send("Player Removed");
 });
-module.exports=app;
+module.exports = app;
+
